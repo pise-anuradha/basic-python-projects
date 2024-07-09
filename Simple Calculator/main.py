b156807397f5ff5c48bbdf103017e8d417839db0
pise-anuradha <167015021+pise-anuradha@users.noreply.github.com>
2024-07-09 16:43:01 +0530

    Update main.py

--- Simple Calculator/main.py
+++ Simple Calculator/main.py
@@ -1,1 +1,21 @@
+print('''
++ ADD
+- SUB
+* MUL
+/ DIV
+''')
 
+num1 = int(input('Enter the Value1:'))
+num2 = int(input('Enter the Value2:'))
+opr = input('Enter the Operator:')
+
+if opr == '+':
+  print(num1+num2)
+elif opr == '-':
+  print(num1-num2)
+elif opr == '*':
+  print(num1*num2)
+elif opr == '/':
+  print(num1/num2)
+else:
+  print('Invalid Operator')
